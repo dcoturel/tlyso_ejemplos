0b6a7b49689d7643554e20e7c345a1a634751350
dcoturel <dcoturel@gmail.com>
2019-02-07 19:44:18 -0300

    Primer commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,152 @@ namespace ConsoleApp1
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int ejercicio = 0;
+			Console.WriteLine("Ingrese el numero de ejercicio a ejecutar");
+			ejercicio = Convert.ToInt32(Console.ReadLine());
+			switch (ejercicio)
+			{
+				case 1:
+					ejercicio1();
+					break;
+				case 2:
+					ejercicio2();
+					break;
+				case 3:
+					ejercicio3();
+					break;
+				case 4:
+					ejercicio4();
+					break;
+			}
+
+		}
+
+		private static void ejercicio1()
+		{
+			String nombre;
+			String direccion;
+			Console.WriteLine("Ingrese por favor su nombre:");
+			nombre = Console.ReadLine();
+			Console.WriteLine("Ingrese por favor su dirección:");
+			direccion = Console.ReadLine();
+			Console.WriteLine("Usted se llama " + nombre + " y vive en " + direccion);
+			Console.WriteLine("Press any key");
+			Console.Read();
+		}
+
+		private static void ejercicio2()
+		{
+			String nombre = "";
+			String direccion = "";
+			Console.WriteLine("Ingrese por favor su nombre:");
+			while (nombre.Length == 0)
+			{
+				nombre = Console.ReadLine();
+				if (nombre.Equals(""))
+				{
+					Console.WriteLine("Debe ingresar un valor para el nombre");
+				}
+			}
+
+			Console.WriteLine("Ingrese por favor su dirección:");
+			while (direccion.Length == 0)
+			{
+				direccion = Console.ReadLine();
+				if (direccion.Equals(""))
+				{
+					Console.WriteLine("Debe ingresar un valor para la direccion");
+				}
+			}
+
+			Console.WriteLine("Usted se llama " + nombre + " y vive en " + direccion);
+			Console.WriteLine("Press any key");
+			Console.Read();
+		}
+
+		private static void ejercicio3()
+		{
+			double numero1;
+			double numero2;
+			double promedio;
+			Console.WriteLine("Ingrese un numero entre 0 y 10");
+			do
+			{
+				numero1 = Convert.ToDouble(Console.ReadLine());
+				if (numero1 < 0 || numero1 > 10)
+				{
+					Console.WriteLine("Dije entre 0 y 10...");
+				}
+			} while (numero1 < 0 || numero1 > 10);
+
+			Console.WriteLine("Ingrese otro numero entre 0 y 10");
+			do
+			{
+				numero2 = Convert.ToDouble(Console.ReadLine());
+				if (numero2 < 0 || numero2 > 10)
+				{
+					Console.WriteLine("Dije entre 0 y 10...");
+				}
+			} while (numero2 < 0 || numero2 > 10);
+
+			promedio = (numero1 + numero2) / 2;
+
+			Console.WriteLine("El promedio entre los numeros ingresados es " + promedio);
+			Console.WriteLine("Press any key");
+			Console.Read();
+		}
+
+		private static void ejercicio4()
+		{
+			int horas;
+			int minutos;
+			int segundos;
+
+			Console.WriteLine("Ingrese el valor para las horas");
+			do
+			{
+				horas = Convert.ToInt32(Console.ReadLine());
+				if (horas < 0 || horas > 23)
+				{
+					Console.WriteLine("Deberia estar entre 0 y 23...");
+				}
+			} while (horas < 0 || horas > 23);
+
+			Console.WriteLine("Ingrese el valor para los minutos");
+			do
+			{
+				minutos = Convert.ToInt32(Console.ReadLine());
+				if (minutos < 0 || minutos > 59)
+				{
+					Console.WriteLine("Deberia estar entre 0 y 59...");
+				}
+			} while (minutos < 0 || minutos > 59);
+
+			Console.WriteLine("Ingrese el valor para los segundos");
+			do
+			{
+				segundos = Convert.ToInt32(Console.ReadLine());
+				if (segundos < 0 || segundos > 59)
+				{
+					Console.WriteLine("Deberia estar entre 0 y 59...");
+				}
+			} while (segundos < 0 || segundos > 59);
+
+			Console.WriteLine("La cantidad total de segundos es " + (horas * 60 * 60 + minutos * 60 + segundos) + "");
+
+			Console.WriteLine("Press any key");
+			Console.Read();
+		}
+
+		private static void ejercicio5()
+		{
+			String variable1;
+			String variable2;
+
+
+
+			Console.WriteLine("Press any key");
+			Console.Read();
 		}
 	}
 }
